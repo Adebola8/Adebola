494adec9874d03a57b9732d01a7e817dcfb6197f
Adebola <62142347+Adebola8@users.noreply.github.com>
2020-03-25 14:00:24 +0100

    Python task1

---
+++ python.py
@@ -0,0 +1,13 @@
+# import math module to get value of pi
+
+import math
+# r is radius of a circle
+#A is area of a circle
+
+r= float(input("Enter a value"))
+
+#calculate Area
+
+A= math.pi*(r**2)
+
+print (A)
